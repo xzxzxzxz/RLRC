ba36a7c4a676472fd3ca925c1b215bf35dcde29e
Chen Tang <chen_tang@berkeley.edu>
2019-02-26 16:07:51 -0800

    add smooth fun

--- src/controller/include/controller/comp_error_core.h
+++ src/controller/include/controller/comp_error_core.h
@@ -12,14 +12,28 @@
 #include <path_follower/Trajectory2D.h>
 #include <path_follower/state_Dynamic.h>
 #include <controller/TrackingInfo.h>
-
 #include <path_follower/Vehicle.h>
+
+//#include "qpOASES/qpOASES.hpp"
+#include <eigen3/Eigen/Core>
+#include <eigen3/Eigen/Geometry>
+#include <eigen3/Eigen/LU>
 #include <math.h>
 #include <vector>
 using namespace std;
 
+/*
+void Array2realt(Eigen::ArrayXd A, real_t *t) {
+    int k = A.size();
+    for(int i=0; i < k; i++)
+        t[i] = A[i];
+}
+*/
+
 float getDistance(const path_follower::TrajectoryPoint2D point, const path_follower::state_Dynamic state, float ra_to_cg, float ds);
 
 vector<float> ComputeLateralError(const path_follower::Trajectory2D traj, const path_follower::state_Dynamic state, float ra_to_cg, float ds);
+
+//path_follower::Trajectory2D Transient(const path_follwoer::Trajectory2D traj, const path_follower::TrajectoryPoint2D point, const path_follower::state_Dynamic state, float ra_to_cg, float ds, float num_points)
 
 #endif
--- src/controller/src/comp_error_core.cpp
+++ src/controller/src/comp_error_core.cpp
@@ -29,7 +29,7 @@ vector<float> ComputeLateralError(const path_follower::Trajectory2D traj, const 
   float ref_v = state.vx;
   float wx = 0;
   float wy = 0;
-  for (int i = (int)traj.point.size() - 1; i >= 1; i--)
+  for (int i = 1; i < (int)traj.point.size(); i++)
   {
     float wx_i = cos(state.psi + beta_s);
     float wy_i = sin(state.psi + beta_s);
@@ -72,3 +72,119 @@ vector<float> ComputeLateralError(const path_follower::Trajectory2D traj, const 
 
   return error_msg_vector;
 }
+/*
+path_follower::Trajectory2D Transient(const path_follwoer::Trajectory2D traj, const path_follower::TrajectoryPoint2D point, const path_follower::state_Dynamic state, float ra_to_cg, float ds, float num_points)
+{
+  float x_cg_ds = state.X + cos(state.psi) * (ra_to_cg + ds);
+  float y_cg_ds = state.Y + sin(state.psi) * (ra_to_cg + ds);
+  float beta_s = atan2(state.vy, state.vx) + ds / state.vx * state.wz; 
+  float ref_x = x_cg_ds;
+  float ref_y = y_cg_ds; 
+  float ref_yaw = 0; 
+  float ref_kappa = 0; 
+  float ref_v = state.vx;
+  float wx = 0;
+  float wy = 0;
+  float index = 0;
+  for (int i = 1; i < (int)traj.point.size(); i++)
+  {
+    float wx_i = cos(state.psi + beta_s);
+    float wy_i = sin(state.psi + beta_s);
+    float p1x = traj.point[i-1].x - x_cg_ds;
+    float p1y = traj.point[i-1].y - y_cg_ds;
+    float p2x = traj.point[i].x - x_cg_ds;
+    float p2y = traj.point[i].y - y_cg_ds;
+    float d1 = p1x * wx_i + p1y * wy_i;
+    float d2 = p2x * wx_i + p2y * wy_i;
+    if (signbit(d1) != signbit(d2) || d1 == 0 || d2 == 0) {
+      index = (abs(d1)>=abs(d2)) ? i : i-1
+      break;
+    }
+  }
+  Eigen::MatrixXd x_ref((num_points+1)*2, 1);
+  x_ref(0, 0) = 0.;
+  x_ref(1, 0) = 0.;
+  for (int i=0; i<num_points; i++)
+  {
+    x_ref((i+1)*2, 0) = traj.point[index+i].x - point.x;
+    x_ref((i+1)*2+1, 0) = traj.point[index+i].y - point.y;
+  }
+  float dt = traj.point[1].t - traj.point[0].t
+
+  float w1 = 1;
+  float w2 = 1;
+  Eigen::MatrixXd V((num_points-1)*2, (num_points+1)*2);
+  Eigen::MatrixXd A((num_points-1)*2, (num_points+1)*2);
+  V.setZero();
+  A.setZero();
+  for (int i=0; i<num_points-1; i++) 
+  {
+    V.block(i*2, (i+1)*2, 2, 2) = - Eigen::Matrix<float, 2, 2>::Identity() / dt;
+    V.block(i*2, (i+2)*2, 2, 2) = Eigen::Matrix<float, 2, 2>::Identity() / dt;
+    A.block(i*2, i*2, 2, 2) = Eigen::Matrix<float, 2, 2>::Identity() / pow(dt, 2);
+    A.block(i*2, (i+1)*2, 2, 2) = - 2 * Eigen::Matrix<float, 2, 2>::Identity() / pow(dt, 2);
+    A.block(i*2, (i+2)*2, 2, 2) = Eigen::Matrix<float, 2, 2>::Identity() / pow(dt, 2);    
+  }
+  Eigen::MatrixXd H((num_points+1)*2, (num_points+1)*2);
+  H = 2 * (Eigen::Matrix<float, (num_points+1)*2, (num_points+1)*2>::Identity() + w1 * V.transpose() * V + w2 * A.transpose() * A)
+  g = - 2 * (Eigen::Matrix<float, (num_points+1)*2, (num_points+1)*2>::Identity() + w1 * V.transpose() * V) * x_ref;
+  ArrayXd aH = Map<const ArrayXd>(H.data(), H.size());
+  ArrayXd ag = Map<const ArrayXd>(g.data(), g.size());
+  real_t tH[aH.size()];
+  real_t tg[ag.size()];
+  Array2realt(aH, tH);
+  Array2realt(ag, tg);
+
+  Eigen::Matrix<float, 6, (num_points+1)*2> Ain;
+  Ain.block(0, 0, 2, 2) = Eigen::Matrix<float, 2, 2>::Identity();
+  Ain.block(2, num_points*2, 2, 2) = Eigen::Matrix<float, 2, 2>::Identity();
+  Ain.block(4, 0, 2, 2) = - Eigen::Matrix<float, 2, 2>::Identity() / dt;
+  Ain.block(4, 2, 2, 2) = Eigen::Matrix<float, 2, 2>::Identity() / dt;
+  ArrayXd aAin = Map<const ArrayXd>(Ain.data(), Ain.size());
+  real_t tAin[aAin.size()];
+  Array2realt(aAin, tAin);
+  real_t lbA[6];
+  real_t ubA[6];
+  lbA[0] = x_ref(0, 0); lbA[1] = x_ref(1, 0); 
+  lbA[2] = x_ref(num_points*2, 0); lbA[3] = x_ref(num_points*2+1, 0); 
+  lbA[4] = point.v * cos(point.theta); lbA[5] = point.v * sin(point.theta);
+  ubA[0] = x_ref(0, 0); ubA[1] = x_ref(1, 0); 
+  ubA[2] = x_ref(num_points*2, 0); ubA[3] = x_ref(num_points*2+1, 0); 
+  ubA[4] = point.v * cos(point.theta); ubA[5] = point.v * sin(point.theta);
+  real_t lb[(num_points+1)*2] = -100;
+  real_t ub[(num_points+1)*2] = 100;
+
+  QProblem example((num_points+1)*2, 6);
+  Options options;
+  example.setOptions(options);
+  int_t nWSR = 100;
+  example.init(tH, tg, tAin, lb, ub, lbA, ubA, nWSR);
+  real_t xOpt[(num_points+1)*2];
+  example.getPrimalSolution(xOpt);
+
+  path_follower::Trajectory2D traj_new;
+  traj_new.point[0] = point;
+  traj_new.point[0].t = 0;
+  for (int i=0; i < (int)traj.point.size() - index; i++)
+  {
+    if (i >= 0 && i < num_points-1)
+    {
+      vx = (xOpt[(i+2)*2] - xOpt[(i+1)*2]) / dt;
+      vy = (xOpt[(i+2)*2+1] - xOpt[(i+1)*2+1]) / dt;
+      ax = (xOpt[i*2] - 2 * xOpt[(i+1)*2] + xOpt[(i+2)*2]) / pow(dt, 2);
+      ay = (xOpt[i*2+1] - 2 * xOpt[(i+1)*2+1] + xOpt[(i+2)*2+1]) / pow(dt, 2);
+      traj_new.point[i+1].t = (i+1)*dt;
+      traj_new.point[i+1].x = xOpt[(i+1)*2];
+      traj_new.point[i+1].y = xOpt[(i+1)*2+1];
+      traj_new.point[i+1].v = sqrt(pow(vx, 2) + pow(vy, 2));
+      traj_new.point[i+1].theta = atan2(vy, vx);
+      traj_new.point[i+1].kappa = (vx*ay - vy*ax) / pow(pow(vx, 2) + pow(vy, 2), 1.5);
+    }
+    else
+    {
+      traj_new.point[i+1] = traj.point[index+i];
+    }
+  }
+  return traj_new;
+}
+*/
